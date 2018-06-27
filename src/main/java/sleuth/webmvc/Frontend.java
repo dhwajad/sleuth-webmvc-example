9b6d8411651809c3059f2e9835789b394f02b3a6
DJ Kulkarni <Dhwajad.Kulkarni@aig.com>
2018-06-27 17:01:10 -0400

    simplified for brevity

--- src/main/java/sleuth/webmvc/Frontend.java
+++ src/main/java/sleuth/webmvc/Frontend.java
@@ -1,10 +1,28 @@
 package sleuth.webmvc;
 
+import brave.Tracer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.Future;
 
 @SpringBootApplication
+@RestController
+@CrossOrigin
 public class Frontend {
+
+  @Autowired
+  Tracer tracer;
+
+  @RequestMapping("/")
+  public String printTraceIds() {
+    return "Current Trace/Span: " + tracer.currentSpan().context().toString() + "<br/><br/> New Trace/Span: " + tracer.newTrace().context().toString();
+
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(Frontend.class,
